20ba3660c29ea0befd77b17351e6407c6b794305
Masumi Murata <masumi.murata@loftwork.com>
2022-09-05 16:22:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,17 +15,20 @@ const asyncLineReader = async (iterater) => {
 }
 (async () => { 
 
-    await asyncLineReader(async (lineString) => {    
-        const response = await axios.get(lineString);
+    await asyncLineReader(async (lineString) => {   
+        //分割してURLとIDに分ける
+        const lineText = lineString.split(',');
+        const response = await axios.get(lineText[1]);
         const $ = await cheerio.load(response.data);
 
-        let url = new URL(lineString);
-        //タイトルの取得
+        let url = new URL(lineText[1]);
+        const id = lineText[0];
+        //取得したい要素の指定と追加HTML
         //cheerioはjqueryライクの記述で使用できる
-        let targetdiv = $('.entryHeader__title').html();
-        console.log(targetdiv);
+        let targetdiv = $('.entry-content').html() + "\n<style>.c-entry__content.p-entry-content p {margin:0}</style>";
+        //console.log(targetdiv);
 
-        let filename = url.pathname.toString().replace(/\//g,'_').substring(1,url.pathname.toString().length);
+        let filename = id + ".txt";
         fs.writeFileSync(filename,targetdiv);
     })
     //
